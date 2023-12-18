e476fd88535b0b7d3f158c2340cc010ad7233b53
yatsenko.v <FDT3sRtwD>
2023-12-18 20:52:32 +0300

    init - 2

--- src/main/java/ru/maxima/controllers/HelloController.java
+++ src/main/java/ru/maxima/controllers/HelloController.java
@@ -1,2 +1,24 @@
-package ru.maxima.controllers;public class HelloController {
+package ru.maxima.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        System.out.println("You are inside your controller");
+        return "hello";
+    }
+
+    @GetMapping("/name")
+    public String sayName() {
+        return "name";
+    }
+
+    @GetMapping("/address")
+    public String sayAddress() {
+        return "home";
+    }
 }
