89b5c625231599632ff9a97e7dda0f21062c14e3
yatsenko.v <FDT3sRtwD>
2024-01-11 18:49:52 +0300

    init - 2

--- src/main/java/ru/maxima/controllers/HelloController.java
+++ src/main/java/ru/maxima/controllers/HelloController.java
@@ -1,16 +1,54 @@
 package ru.maxima.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello-world")
-    public String sayHello() {
+    // CRUD - create read update delete
+    // @PostMapping - create
+    // @GetMapping - read
+    // @PutMapping - update
+    // @PatchMapping - update
+    // @DeleteMapping - delete
+
+    @GetMapping("/old-hello-world")
+    public String oldSayHello(HttpServletRequest request,
+                              Model model) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        Integer age = Integer.valueOf(request.getParameter("age"));
+
+        model.addAttribute("message", "Hello, " + name + ", " + surname);
+        model.addAttribute("myAge", "My age is " + age + " years");
+
+        System.out.println(name + " : " + surname);
         System.out.println("You are inside your controller");
         return "hello";
     }
+
+    @GetMapping("/new-hello-world")
+    public String newSayHello(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("You are inside your controller");
+        return "hello";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     @GetMapping("/name")
     public String sayName() {
